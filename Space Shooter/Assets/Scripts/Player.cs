d2f8bcd7cd39be51b5329e46550dbfc727f2b82c
Omar-Castorena <48456501+ocastorena@users.noreply.github.com>
2022-01-24 22:12:20 -0700

    Add player movement
    Added player movement including controller, speed, and player constraints

--- Space Shooter/Assets/Scripts/Player.cs
+++ Space Shooter/Assets/Scripts/Player.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField]
-    private float _speed = 3.5f;
+    private float _speed = 5.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        // take the current position = new position (0, 0, 0)
+        // player position when starting
         transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * _speed *  Time.deltaTime);
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        // getting input values from Input Manager: horizontal and vertical
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // updating transform positions (x,y) with user input
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        // clamps player position on the y
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -4, 0), 0);
+
+        // wraps player around if past the bounds on the x
+        if (transform.position.x >= 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
